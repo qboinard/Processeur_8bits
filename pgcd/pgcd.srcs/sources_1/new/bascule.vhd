77324a4d50d854b0e1bb3eeef14e68d097935d52
lthuillie002 <74877406+lthuillie002@users.noreply.github.com>
2021-02-24 09:36:57 +0100

    bascule

--- pgcd/pgcd.srcs/sources_1/new/bascule.vhd
+++ pgcd/pgcd.srcs/sources_1/new/bascule.vhd
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------------
--- Company: 
+-- Company:
 -- Engineer: 
 -- 
 -- Create Date: 02/24/2021 09:15:40 AM
@@ -18,24 +18,40 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity bascule is
---  Port ( );
+    
+    port
+    (
+        CE                  : in    std_logic;
+        H                   : in    std_logic;
+        RST                 : in    std_logic;
+        Load_registre_data  : in    std_logic;
+
+        data_in             : in    std_logic_vector(7 downto 0);
+        data_out            : out   std_logic_vector(7 downto 0)
+    );
+
 end bascule;
 
 architecture Behavioral of bascule is
+
+    Process (H, RST)
+    Begin
+        if (RST = '1') then
+            data_out <= (others=>'0');
+
+        elsif (H'event and H = '1') then
+            if (CE = '1') then
+                if (Load_registre_data = '1') then
+                    data_out <= data_in;
+                end if;
+            end if;
+
+        end if;
+    end Process;
 
 begin
 
