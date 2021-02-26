e0ff0d90013d802d0b126cec7f4334ba74df410d
qboinard <q.boinard@gmail.com>
2021-02-26 08:24:13 +0100

    Creation de uc, ut et cpu

---
+++ pgcd/pgcd.srcs/sources_1/new/ut.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/26/2021 08:23:12 AM
+-- Design Name: 
+-- Module Name: ut - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ut is
+--  Port ( );
+end ut;
+
+architecture Behavioral of ut is
+
+begin
+
+
+end Behavioral;
